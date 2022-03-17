8f6fcec6153b8feed91017cd4de56dadd8c9e497
Haley Siharath <37295441+HaleySiharath@users.noreply.github.com>
2022-03-17 14:00:13 -0400

    fixed app.py function issues

--- app.py
+++ app.py
@@ -13,27 +13,23 @@ def index():
 
 
 @app.get('/signin')
-def sign():
+def signin():
     return render_template('signin.html')
 
 @app.get('/signup')
-def sign():
+def signup():
     return render_template('signup.html')
 
 @app.get('/profile')
-def sign():
+def profile():
     return render_template('profile.html')
 
 
 @app.get('/viewpost')
-def sign():
+def viewpost():
     return render_template('viewpost.html')
 
 
 @app.get('/createpost')
-def sign():
+def createpost():
     return render_template('createpost.html')
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
