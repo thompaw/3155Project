9a7bfa8bd5fcd63fbc81bfb364617c5e954b805d
Haley Siharath <37295441+HaleySiharath@users.noreply.github.com>
2022-03-17 13:33:36 -0400

    Added files needed for application

--- app.py
+++ app.py
@@ -2,18 +2,37 @@ from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
-
 @app.get('/')
-def index():
+def home():
     return render_template('index.html')
 
+
 @app.get('/home')
-def home():
+def index():
     return render_template('home.html')
 
-@app.get('/signpage')
+
+@app.get('/signin')
 def sign():
-    return render_template('signpage.html')
+    return render_template('signin.html')
+
+@app.get('/signup')
+def sign():
+    return render_template('signup.html')
+
+@app.get('/profile')
+def sign():
+    return render_template('profile.html')
+
+
+@app.get('/viewpost')
+def sign():
+    return render_template('viewpost.html')
+
+
+@app.get('/createpost')
+def sign():
+    return render_template('createpost.html')
 
 
 if __name__ == "__main__":
