57827a2adfd9482b993e418d2cfedbbff8f0e0cb
hirdhaykalra <hirdhay@gmail.com>
2022-03-18 07:53:28 -0400

    Temporary Sign-In page + navbar changes

--- app.py
+++ app.py
@@ -1,37 +1,37 @@
 from flask import Flask, redirect, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__) #static_url_path='/static' (??) (ignore)
 
 @app.get('/')
-def home():
+def index():
     return render_template('index.html')
 
 
 @app.get('/home')
-def index():
+def home():
     return render_template('home.html')
 
 
 @app.get('/signin')
-def sign():
-    return render_template('signin.html')
+def signin():
+    return render_template('signin.html', )
 
 @app.get('/signup')
-def sign():
+def singup():
     return render_template('signup.html')
 
 @app.get('/profile')
-def sign():
+def profile():
     return render_template('profile.html')
 
 
 @app.get('/viewpost')
-def sign():
+def viewpost():
     return render_template('viewpost.html')
 
 
 @app.get('/createpost')
-def sign():
+def createpost():
     return render_template('createpost.html')
 
 
