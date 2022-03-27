f64d685b778b3f0028ec81ae2097983ab22ac8b4
Thomas Pawlowski <98345171+thompaw@users.noreply.github.com>
2022-03-27 15:00:25 -0400

    created page for viewing post, added global lists to keep track of users, posts, songs. made a TODO for viewpost function

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__) #static_url_path='/static' (??) (ignore)
 
+# placeholder lists of dictionaries till sql implementation
+users = []  # each user has a 'name' and 'status'
+posts = []  # posts have a 'title', 'caption' and 'song'
+songlist = []  # songs have a 'title' and 'artist'
+
+
+
 @app.get('/')
 def index():
     return render_template('index.html')
@@ -24,10 +31,16 @@ def signup():
 def profile():
     return render_template('profile.html')
 
-
 @app.get('/viewpost')
 def viewpost():
-    return render_template('viewpost.html')
+    # TODO figure out which post is being viewed, and find the corresponding user. 
+    # TODO then find the song being listed in the post
+    # TODO input these to the template
+    return render_template('viewpost.html', user= users[0], post= posts[0], song=songlist[0])
+
+@app.post('/createpost')
+def createpost():
+    return render_template('createpost.html')
 
 
 @app.get('/createpost')
