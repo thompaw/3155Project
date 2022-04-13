e3a127fac2cd44dc8d4c13489978a4dd3eaba988
hirdhaykalra <hirdhay@gmail.com>
2022-04-13 18:12:37 -0400

    assigning tasks

--- blueprints/user_profile_blueprint.py
+++ blueprints/user_profile_blueprint.py
@@ -5,30 +5,37 @@ from models import User_Profile, db
 
 router = Blueprint('user_profile_router', __name__, url_prefix='/user_profile')
 
+# Hirdhay
 @router.get('')
 def get_all_user_profile():
     pass
 
+# Haley
 @router.get('/<user_id>')
 def get_single_user_profile(user_id):
     pass
 
+# Haley
 @router.get('/new')
 def create_user_profile_form():
     pass
 
+# Haley
 @router.post('')
 def create_user_profile():
     pass
 
+# Hirdhay
 @router.get('/<user_id>/edit')
 def get_edit_user_profile_form(user_id):
     pass
 
+# Hirdhay
 @router.post('/<user_id>')
 def update_user_profile(user_id):
     pass
 
+# Hirdhay
 @router.post('/<user_id>/delete')
 def delete_user_profile(user_id):
     pass
