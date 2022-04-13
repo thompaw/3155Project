cc327a65b09f3585bc8d5d973775737c74b49037
Haley Siharath <37295441+HaleySiharath@users.noreply.github.com>
2022-04-13 18:03:06 -0400

    template for user_profile CRUD

--- blueprints/user_profile_blueprint.py
+++ blueprints/user_profile_blueprint.py
@@ -1,2 +1,34 @@
 # Implement all CRUD elements
 # Reference this: https://github.com/jacobtie/itsc-3155-module-10-demo/blob/main/blueprints/book_blueprint.py
+from flask import Blueprint, abort, redirect, render_template, request
+from models import User_Profile, db
+
+router = Blueprint('user_profile_router', __name__, url_prefix='/user_profile')
+
+@router.get('')
+def get_all_user_profile():
+    pass
+
+@router.get('/<user_id>')
+def get_single_user_profile(user_id):
+    pass
+
+@router.get('/new')
+def create_user_profile_form():
+    pass
+
+@router.post('')
+def create_user_profile():
+    pass
+
+@router.get('/<user_id>/edit')
+def get_edit_user_profile_form(user_id):
+    pass
+
+@router.post('/<user_id>')
+def update_user_profile(user_id):
+    pass
+
+@router.post('/<user_id>/delete')
+def delete_user_profile(user_id):
+    pass
