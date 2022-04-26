d1b5d001131e7187179ee25e498710bfa367f433
Thomas Pawlowski <98345171+thompaw@users.noreply.github.com>
2022-04-26 08:28:25 -0400

    added comments, need to do redesign for front end

--- blueprints/comment_blueprint.py
+++ blueprints/comment_blueprint.py
@@ -9,49 +9,49 @@ router = Blueprint('Comment_router', __name__, url_prefix='/Comment')
 # CREATE
 @router.get('/new_comment')
 def create_user_profile_form():
-    return render_template('signup.html')
+    return render_template('signup.html')  # return the user with the new comment form
 
 @router.post('')
-def create_comment():
+def create_comment():  # taking data from the form
     postnum = None
-    usernum = None
+    usernum = None  # should auto grab these based on where the form is located
     goop = request.form.get('content', '')
 
-    new_comment = Comment(user_id=usernum, post_id=postnum, content=goop)
+    new_comment = Comment(user_id=usernum, post_id=postnum, content=goop)  # create a new comment object using the data from the form
     db.session.add(new_comment)
-    db.session.commit()
+    db.session.commit()  # save changes and commit to db
 
-    return redirect(f'Comment/<comment_id>')
+    return redirect(f'Comment/<comment_id>')  # send user to their new comment
 
 # READ
 @router.get('')  # per post
-def get_all_comments(post_id):
+def get_all_comments(post_id):  # grab all comments associated to a single post
     pass
 
 @router.get('/<comment_id>')  # single comment
-def get_single_comment(comment_id):
+def get_single_comment(comment_id):  # grab a single comment by it's id
     pass
 
 # UPDATE
 @router.post('/<comment_id>')
 def update_comment(comment_id):
-    updating_comment = Comment.query.get_or_404(comment_id)
+    updating_comment = Comment.query.get_or_404(comment_id)  # get the object to be updated
 
-    goop = request.form.get('content', '')
+    goop = request.form.get('content', '')  # get the new content from the edit form, yet to be implemented
 
-    if len(goop) < 1:
+    if goop == '':  # if the content of the comment is empty, don't accept it
         abort(400)
 
-    updating_comment.content = goop
+    updating_comment.content = goop  # change the old to the new
 
-    db.session.commit()
+    db.session.commit()  # commit changes to the db
 
-    return redirect(f'Comment/<comment_id>')
+    return redirect(f'Comment/<comment_id>') # send the user to their new comment
 
 # DELETE
 @router.post('/<comment_id>/delete')
 def delete_comment(comment_id):
-    comment_to_delete = Comment.query.get_or_404(comment_id)
-    db.session.delete(comment_to_delete)
+    comment_to_delete = Comment.query.get_or_404(comment_id)  # grab the comment id 
+    db.session.delete(comment_to_delete)  # delete the comment and then save changes. 
     db.session.commit()
-    return redirect('/<post_id>')
+    return redirect('/<post_id>')  # return to the post that comment was on
